a30b83eebfc0edcf63c88eed44e0eb372f43962d
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-01-20 14:31:33 -0800

    inistal commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//Import the express module
+import express from "express";
+
+//Create an instance of an Express application
+const app = express();
+
+// Define the port number where our server will listen
+const PORT = 3000;
+
+// Define the root path ('/')
+app.get("/", (req, res) => {
+  // Semd text as a response to the client
+  res.send("Hola, Mundo!");
+});
+
+// State the server and tell it listen on the port
+// specified above. Note the backticks!
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
